ece0a82c1a9be68a883b6ded3eda19e4b0850e4a
liqining <liqining.keeling@bytedance.com>
2023-01-14 02:36:49 +0800

    feat: add mini chromium base library

--- test.cc
+++ test.cc
@@ -1,8 +1,9 @@
 #include <iostream>
 #include "core/js_runtime.h"
-using namespace std;
+#include "base/logging.h"
 
 int main() {
+    LOG(ERROR) << "keilingnica " << __func__;
     nica::JSRuntime runtime;
     nica::JSRuntime::Scope runtime_scope(&runtime);
     runtime.EvaluateJavascriptSource("'Hello' + ', World!'");
