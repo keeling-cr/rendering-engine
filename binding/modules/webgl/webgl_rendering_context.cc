b857ff77fc3ff8c000ecb2f293710a5cc2ea9f2f
liqining <liqining.keeling@bytedance.com>
2023-02-01 12:19:57 +0800

    bugfix: [binding] fix compile error

--- binding/modules/webgl/webgl_rendering_context.cc
+++ binding/modules/webgl/webgl_rendering_context.cc
@@ -1,6 +1,5 @@
 #include "binding/modules/webgl/webgl_rendering_context.h"
 #include "base/logging.h"
-#include "binding/modules/webgl/webgl_texture.h"
 
 namespace bind {
 
@@ -14,11 +13,6 @@ WebGLRenderingContext:: WebGLRenderingContext(v8::Isolate* isolate)
     , context_id_(s_context_counter++) {}
 
 WebGLRenderingContext::~WebGLRenderingContext() = default;
-
-WebGLTexture* WebGLRenderingContext::CreateTexture(GLuint texture_id) {
-  WebGLTexture* texture = new WebGLTexture(this, texture_id);
-  return texture;
-}
 
 nica::FunctionTemplateBuilder 
 WebGLRenderingContext::GetFunctionTemplateBuilder(
